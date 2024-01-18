4d4678de5e574512b9fb1ff760e7e80509eaab76
JustinL <justinpltp@gmail.com>
2024-01-18 23:51:42 +0800

    Added very basic movement & some platforms

---
+++ My First World/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    // for horizontal movement
+    public float movementspeed;
+    private float horizontalmovement;
+
+    //for jumping & groundcheck
+    public float jumpheight;
+    //private bool isgrounded; //to check if player is grounded
+    public Vector2 boxsize; // for the size of the box to be use in boxcasting
+    public float castdistance; //for the cast distance from the centre position of the gameobject 
+    public LayerMask layer; // for the layer you want to detect, in this case will be all platform 
+
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //for horizontal movement
+        horizontalmovement = Input.GetAxisRaw("Horizontal");
+        body.velocity = new Vector2(horizontalmovement * movementspeed, body.velocity.y);
+
+        if((Input.GetKeyUp(KeyCode.A) == true || Input.GetKeyUp(KeyCode.D) == true ) && isgrounded() == true) //to make character not bounce when going up slope
+        {
+            body.velocity = new Vector2(0, 0);
+        }
+
+        //for jumping
+        /*isgrounded = isgrounded();*/
+        if (Input.GetButtonDown("Jump") == true && isgrounded() == true)
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpheight);
+        }
+        if (horizontalmovement == 0 && isgrounded() == true)
+        {
+            body.gravityScale = 0;
+        }
+        else body.gravityScale = 2;//lol
+    }
+
+    //function for checking if player is grounded
+    public bool isgrounded()
+    {
+        if (Physics2D.BoxCast(transform.position, boxsize, 0, -transform.up, castdistance, layer))
+        {
+            return true;
+        }
+        else return false;
+    }
+    /*public void OnDrawGizmos() // to for groundcheck/isgrounded
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castdistance, boxsize);
+    }*/
+}
