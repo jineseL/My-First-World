b1e1aa70645de4bda330aa88a30895ee314c6bdb
daphnnechua <daphnnechua@gmail.com>
2024-02-26 16:53:58 +0800

    doublelvl update

---
+++ My First World/Assets/Scripts/SlowballScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowballScript : MonoBehaviour
+{
+    public float rainaccleration;
+    private Rigidbody2D body;
+
+    private float timer;
+    private float rainballdespawntimer = 3;
+    public float rainballspeed;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < rainballdespawntimer)
+        {
+            timer += Time.deltaTime;
+        }
+        else Destroy(gameObject);
+    }
+    private void FixedUpdate()
+    {
+        /*rainaccleration++;
+
+        transform.position = new Vector3(transform.position.x ,transform.position.y,transform.position.z);*/
+        body.gravityScale += rainaccleration;
+        if (body.gravityScale > rainballspeed)
+        {
+            body.gravityScale = rainballspeed;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Player"))
+        {
+            collider.GetComponent<PlayerHealth>().damage(transform.position);
+            Destroy(gameObject);
+        }
+        /*if (collider.CompareTag("Platform"))
+        {
+            Destroy(gameObject);
+        }*/
+    }
+}
